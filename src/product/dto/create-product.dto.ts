43e0d59aa9d06e81be8df4443a44c324f2f1e1b1
arrdix <mail.ardix@gmail.com>
2024-07-16 20:09:58 +0700

    feat: improve create product service

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,29 +1,35 @@
-import {
-  IsArray,
-  IsBoolean,
-  IsNumber,
-  IsOptional,
-  IsString,
-} from "class-validator";
+import { Type } from 'class-transformer'
+import { IsArray, IsBoolean, IsNumber, IsOptional, IsString, ValidateNested } from 'class-validator'
+import { CreateVariantDto } from 'src/variant/dto/create-variant.dto'
 
 export class CreateProductDto {
-  @IsString()
-  name: string;
+    @IsString()
+    name: string
 
-  @IsString()
-  @IsOptional()
-  description: string;
+    @IsString()
+    description: string
 
-  @IsArray()
-  @IsString({ each: true })
-  attachments: string[];
+    @IsArray()
+    @IsString({ each: true })
+    attachments: string[]
 
-  @IsBoolean()
-  isActive: boolean;
+    @IsOptional()
+    @IsBoolean()
+    isActive: boolean
 
-  @IsNumber()
-  minimumOrder: number;
+    @IsNumber()
+    minimumOrder: number
 
-  @IsNumber()
-  storeId: number;
+    @IsNumber()
+    storeId: number
+
+    @IsNumber()
+    categoryId: number
+
+    @IsString()
+    url: string
+
+    @ValidateNested({ each: true })
+    @Type(() => CreateVariantDto)
+    variant: CreateVariantDto
 }
--- src/variant/dto/create-variant.dto.ts
+++ src/variant/dto/create-variant.dto.ts
@@ -1,25 +1,16 @@
-import {
-  IsBoolean,
-  IsInt,
-  IsString,
-  IsOptional,
-  ValidateNested,
-} from "class-validator";
-import { Type } from "class-transformer";
-import { CreateVariantOptionDto } from "../../variant-option/dto/create-variant-option.dto";
+import { IsBoolean, IsOptional, IsString, ValidateNested } from 'class-validator'
+import { Type } from 'class-transformer'
+import { CreateVariantOptionDto } from '../../variant-option/dto/create-variant-option.dto'
 
 export class CreateVariantDto {
-  @IsString()
-  name: string;
+    @IsString()
+    name: string
 
-  @IsBoolean()
-  isActive: boolean;
+    @IsOptional()
+    @IsBoolean()
+    isActive: boolean
 
-  @IsOptional()
-  @IsInt()
-  productId?: number;
-
-  @ValidateNested({ each: true })
-  @Type(() => CreateVariantOptionDto)
-  variantOptions: CreateVariantOptionDto[];
+    @ValidateNested({ each: true })
+    @Type(() => CreateVariantOptionDto)
+    variantOptions: CreateVariantOptionDto[]
 }

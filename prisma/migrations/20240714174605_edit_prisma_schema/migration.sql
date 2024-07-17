cfea2430333225bb4dc8860f4adcd266486ec8ed
petrushandika <petrushandikasinaga@gmail.com>
2024-07-17 14:29:49 +0700

    test: migration schema

--- prisma/migrations/20240714174605_edit_prisma_schema/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Categories" ADD COLUMN     "subcategoryId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Categories" ADD CONSTRAINT "Categories_subcategoryId_fkey" FOREIGN KEY ("subcategoryId") REFERENCES "Categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;

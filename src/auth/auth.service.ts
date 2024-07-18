d1ece49b097d96ebaf970fb9418731b8abcea0ba
arrdix <mail.ardix@gmail.com>
2024-07-19 01:59:49 +0700

    feat: exclude some routes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,129 +1,129 @@
-import { Injectable } from "@nestjs/common";
-import { LoginDto } from "src/auth/dto/login.dto";
-import Hasher from "src/utils/Hasher";
-import { mailer } from "src/utils/Mailer";
-import { RegisterDto } from "src/auth/dto/register.dto";
-import { PrismaService } from "src/prisma/prisma.service";
-import { CartService } from "src/cart/cart.service";
-import { User } from "src/types/user-type";
-import { UserService } from "src/user/user.service";
-import * as jwt from "jsonwebtoken";
-import CONFIG from "src/configs/config";
-import { ForgotPasswordDto } from "src/auth/dto/forgot-password.dto";
-import { ResetPasswordDto } from "src/auth/dto/reset-password.dto";
+import { Injectable } from '@nestjs/common'
+import { LoginDto } from 'src/auth/dto/login.dto'
+import Hasher from 'src/utils/Hasher'
+import { mailer } from 'src/utils/Mailer'
+import { RegisterDto } from 'src/auth/dto/register.dto'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { CartService } from 'src/cart/cart.service'
+import { User } from 'src/types/user-type'
+import { UserService } from 'src/user/user.service'
+import * as jwt from 'jsonwebtoken'
+import CONFIG from 'src/configs/config'
+import { ForgotPasswordDto } from 'src/auth/dto/forgot-password.dto'
+import { ResetPasswordDto } from 'src/auth/dto/reset-password.dto'
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly prismaService: PrismaService,
-    private readonly cartService: CartService,
-    private readonly userService: UserService
-  ) {}
+    constructor(
+        private readonly prismaService: PrismaService,
+        private readonly cartService: CartService,
+        private readonly userService: UserService
+    ) {}
 
-  async register(registerDto: RegisterDto) {
-    // hash the password
-    const hashedPassword = await Hasher.hashPassword(registerDto.password);
+    async register(registerDto: RegisterDto) {
+        // hash the password
+        const hashedPassword = await Hasher.hashPassword(registerDto.password)
 
-    // create new user
-    const createdUser: User = await this.prismaService.users.create({
-      data: {
-        ...registerDto,
-        password: hashedPassword,
-      },
-    });
+        // create new user
+        const createdUser: User = await this.prismaService.users.create({
+            data: {
+                ...registerDto,
+                password: hashedPassword,
+            },
+        })
 
-    // create profile for new user
-    this.createProfile(createdUser.id);
+        // create profile for new user
+        this.createProfile(createdUser.id)
 
-    // create default cart for new user
-    this.cartService.create({
-      price: 0,
-      discount: 0,
-      userId: createdUser.id,
-      storeId: null,
-    });
+        // create default cart for new user
+        this.cartService.create({
+            price: 0,
+            discount: 0,
+            userId: createdUser.id,
+            storeId: null,
+        })
 
-    return {
-      status: "Ok!",
-    };
-  }
-
-  async createProfile(userId: number) {
-    return await this.prismaService.profile.create({
-      data: {
-        userId,
-      },
-    });
-  }
-
-  async login(loginDto: LoginDto) {
-    const requestedUser: User = await this.prismaService.users.findFirst({
-      where: {
-        email: loginDto.email,
-      },
-    });
-
-    if (!requestedUser) {
-      return {
-        status: "Wrong username/password.",
-      };
+        return {
+            status: 'Ok!',
+        }
     }
 
-    const isPasswordMatch = await Hasher.comparePassword(
-      loginDto.password,
-      requestedUser.password
-    );
-
-    if (!isPasswordMatch) {
-      return {
-        status: "Wrong username/password.",
-      };
+    async createProfile(userId: number) {
+        return await this.prismaService.profile.create({
+            data: {
+                userId,
+            },
+        })
     }
 
-    delete requestedUser.email;
-    delete requestedUser.phone;
-    delete requestedUser.password;
+    async login(loginDto: LoginDto) {
+        const requestedUser: User = await this.prismaService.users.findFirst({
+            where: {
+                email: loginDto.email,
+            },
+        })
 
-    return {
-      token: jwt.sign(requestedUser, CONFIG.SECRET_SAUCE),
-    };
-  }
+        if (!requestedUser) {
+            return {
+                status: 'Wrong username/password.',
+            }
+        }
 
-  async forgot(forgotPasswordDto: ForgotPasswordDto) {
-    const requestedUser = await this.prismaService.users.findFirst({
-      where: {
-        email: forgotPasswordDto.email,
-      },
-    });
+        const isPasswordMatch = await Hasher.comparePassword(
+            loginDto.password,
+            requestedUser.password
+        )
 
-    if (!requestedUser) {
-      return {
-        error: `User with email ${forgotPasswordDto.email} doesn't exist.`,
-      };
+        if (!isPasswordMatch) {
+            return {
+                status: 'Wrong username/password.',
+            }
+        }
+
+        delete requestedUser.email
+        delete requestedUser.phone
+        delete requestedUser.password
+
+        return {
+            token: jwt.sign(requestedUser, CONFIG.SECRET_SAUCE),
+        }
     }
 
-    // call send email
-    mailer(forgotPasswordDto.email);
+    async forgot(forgotPasswordDto: ForgotPasswordDto) {
+        const requestedUser = await this.prismaService.users.findFirst({
+            where: {
+                email: forgotPasswordDto.email,
+            },
+        })
 
-    delete requestedUser.email;
-    delete requestedUser.phone;
-    delete requestedUser.password;
+        if (!requestedUser) {
+            return {
+                error: `User with email ${forgotPasswordDto.email} doesn't exist.`,
+            }
+        }
 
-    return {
-      token: jwt.sign(requestedUser, CONFIG.SECRET_SAUCE),
-    };
-  }
+        // call send email
+        mailer(forgotPasswordDto.email)
 
-  async reset(resetPasswordDto: ResetPasswordDto) {
-    const requestedUser = await this.prismaService.users.update({
-      where: {
-        email: resetPasswordDto.requester,
-      },
-      data: {
-        password: await Hasher.hashPassword(resetPasswordDto.password),
-      },
-    });
+        delete requestedUser.email
+        delete requestedUser.phone
+        delete requestedUser.password
 
-    return requestedUser;
-  }
+        return {
+            token: jwt.sign(requestedUser, CONFIG.SECRET_SAUCE),
+        }
+    }
+
+    async reset(resetPasswordDto: ResetPasswordDto) {
+        const requestedUser = await this.prismaService.users.update({
+            where: {
+                email: resetPasswordDto.requester,
+            },
+            data: {
+                password: await Hasher.hashPassword(resetPasswordDto.password),
+            },
+        })
+
+        return requestedUser
+    }
 }

0809f10d7ba95c57c245c79daf7d60df377802fe
arrdix <mail.ardix@gmail.com>
2024-07-25 19:17:10 +0700

    chore: prepare deployment

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import CONFIG from './configs/config'
+// import CONFIG from './configs/config'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import * as express from 'express'
+
+const server = express()
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server))
     app.enableCors()
-
-    await app.listen(CONFIG.PORT)
+    await app.init() // Initialize the app without listening to a port
 }
+
+// Call the bootstrap function
 bootstrap()
 
-export default bootstrap
+// Export the server for Vercel to use
+export const app = server

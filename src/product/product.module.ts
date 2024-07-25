576ff7558ad706265b4fd409d7187300d365c73d
arrdix <mail.ardix@gmail.com>
2024-07-25 16:28:26 +0700

    chore: configure vercel

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { ProductService } from './product.service'
 import { ProductController } from './product.controller'
 import { PrismaService } from 'src/prisma/prisma.service'
-import { CloudinaryService } from 'src/cloudinary/cloudinary.service'
+import { CloudinaryService } from '../cloudinary/cloudinary.service'
 import { CategoryService } from 'src/category/category.service'
 import { StoreService } from 'src/store/store.service'
 
